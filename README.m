42e2426bdadfb307f5b9b5e14f22d6228dafb48a
Muhammad Junaid <70313498+junaid-choudhary15@users.noreply.github.com>
2020-11-26 17:37:19 +0500

    Create README.m

---
+++ README.m
@@ -0,0 +1,15 @@
+BMI Calculator with React Native expo cli
+Install npm modules
+if you are beginner then must check  https://reactnative.dev/docs/getting-started
+
+if you installed expo CLI then run the commans 'npm install'
+'expo start'
+download the expo app from play store and scan the QR code
+
+As this project implemented with android so run below command to run application
+
+react-native run-android
+
+As this project implemented with ios so run below command to run application
+
+react-native run-ios 
